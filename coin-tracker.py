e5395326d710c75f71761126f83cf31b72e1dfdd
Ada <adadatalynn@gmail.com>
2024-11-29 00:41:04 -0600

    Move some code around, add comments

--- coin-tracker.py
+++ coin-tracker.py
@@ -1,5 +1,3 @@
-# This example requires the 'members' and 'message_content' privileged intents to function.
-
 import discord
 import os
 import random
@@ -9,10 +7,7 @@ from discord.ext import commands
 from discord import app_commands
 from typing import Optional
 
-description = '''An example bot to showcase the discord.ext.commands extension
-module.
-
-There are a number of utility commands being showcased here.'''
+description = '''Bot to track user's counts of S&P Coins as well as the current market value'''
 
 intents = discord.Intents.default()
 intents.members = True
@@ -20,8 +15,8 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='?', description=description, intents=intents)
 
+# initial values, although they are overwritten in on_ready if the relevant files exist
 user_coin_counts = {}
-
 last_value = 1000
 
 @bot.event
@@ -41,19 +36,6 @@ async def on_ready():
     print(f'Loaded last value: {last_value}')
     f.close()
     print('------')
-
-@bot.tree.command(
-    name="wallet",
-    description="Tells you how many S&P Coins you have"
-)
-@app_commands.describe(member='The member you want to see the wallet of; defaults to the user who uses the command')
-async def wallet(interaction, member: Optional[discord.Member] = None):
-    member = member or interaction.user
-    count = user_coin_counts.get(str(member.id), 0) # since we save these to JSON, they need to be stringifed
-    coin_message = f'{member.mention} has {count} S&P Coins!' if count != 1 else f'{member.mention} has 1 S&P Coin!'
-    formatted_liquid = '${:,.2f}'.format(count * last_value)
-    message = coin_message + f' ({formatted_liquid} USD)'
-    await interaction.response.send_message(message)
 
 def get_new_value():
     procced = random.randint(0, 100) == 1
@@ -86,6 +68,52 @@ def write_value_to_file():
     f.write(str(last_value))
     f.close()
 
+def write_user_coin_counts_to_file():
+    f = open("user_coin_counts.txt", "w")
+    f.write(json.dumps(user_coin_counts))
+    f.close()
+
+"""
+We use on_raw_reaction_add/remove instead of on_reaction_add/remove as the docs suggest that
+there are cases on_reaction_add isn't called (when the message is not in the cache)
+"""
+
+@bot.event
+async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
+    if (payload.emoji.name == os.environ['COIN_EMOJI_NAME']):
+        if (payload.user_id == payload.message_author_id):
+            return # we ignore coins added by the user that sent the message
+        message_author_id = payload.message_author_id
+        user_coin_counts[str(message_author_id)] = user_coin_counts.get(str(message_author_id), 0) + 1
+        write_user_coin_counts_to_file()
+
+@bot.event
+async def on_raw_reaction_remove(payload: discord.RawReactionActionEvent):
+    if(payload.emoji.name == os.environ['COIN_EMOJI_NAME']):
+        # only on_raw_reaction_add has the message property for some reason, so we have to manually fetch it here
+        channel = bot.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        message_author_id = message.author.id
+        if (payload.user_id == message_author_id):
+            return # we ignore coins added by the user that sent the message
+        
+        count = user_coin_counts.get(message_author_id, 1)
+        user_coin_counts[str(message_author_id)] = count - 1 if count > 0 else 0
+        write_user_coin_counts_to_file()
+
+@bot.tree.command(
+    name="wallet",
+    description="Tells you how many S&P Coins you or another user have"
+)
+@app_commands.describe(member='The member you want to see the wallet of; defaults to the user who uses the command')
+async def wallet(interaction, member: Optional[discord.Member] = None):
+    member = member or interaction.user
+    count = user_coin_counts.get(str(member.id), 0)
+    coin_message = f'{member.mention} has {count} S&P Coins!' if count != 1 else f'{member.mention} has 1 S&P Coin!'
+    formatted_liquid = '${:,.2f}'.format(count * last_value)
+    message = coin_message + f' ({formatted_liquid} USD)'
+    await interaction.response.send_message(message)
+
 @bot.tree.command(
     name="value",
     description="Fetches the current market value of S&P Coin"
@@ -108,35 +136,5 @@ async def value(interaction):
     write_value_to_file()
 
     await interaction.response.send_message(message)
-
-def write_user_coin_counts_to_file():
-    f = open("user_coin_counts.txt", "w")
-    f.write(json.dumps(user_coin_counts))
-    f.close()
-
-@bot.event
-async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
-    """"""
-    if (payload.emoji.name == os.environ['COIN_EMOJI_NAME']):
-        if (payload.user_id == payload.message_author_id):
-            print('Ignoring coin added by the same user')
-            return
-        message_author_id = payload.message_author_id
-        user_coin_counts[str(message_author_id)] = user_coin_counts.get(str(message_author_id), 0) + 1
-        write_user_coin_counts_to_file()
-
-@bot.event
-async def on_raw_reaction_remove(payload: discord.RawReactionActionEvent):
-    """"""
-    if(payload.emoji.name == os.environ['COIN_EMOJI_NAME']):
-        channel = bot.get_channel(payload.channel_id)
-        message = await channel.fetch_message(payload.message_id)
-        message_author_id = message.author.id
-        if (payload.user_id == message_author_id):
-            print('Ignoring coin removed by the same user')
-            return
-        count = user_coin_counts.get(message_author_id, 1)
-        user_coin_counts[str(message_author_id)] = count - 1 if count > 0 else 0
-        write_user_coin_counts_to_file()
 
 bot.run(os.environ['S_P_500_KEY'])
